abf1b2a021ad668e4cb4d98938c3e845e00cd62d
Abdullah Binmahfouz <abinm670@mtroyal.ca>
2020-02-06 09:47:32 +0300

    Updates

---
+++ precache-manifest.a66f26472004804182ee27aefc23968c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ec675cd8395de214cfe9abdec19130",
+    "url": "/back-up_FrontEnd_showMeSaudi/index.html"
+  },
+  {
+    "revision": "3ed8a71628a063dd8be0",
+    "url": "/back-up_FrontEnd_showMeSaudi/static/css/2.5676b8fc.chunk.css"
+  },
+  {
+    "revision": "6df784c21e5959e05962",
+    "url": "/back-up_FrontEnd_showMeSaudi/static/css/main.bcd4551e.chunk.css"
+  },
+  {
+    "revision": "3ed8a71628a063dd8be0",
+    "url": "/back-up_FrontEnd_showMeSaudi/static/js/2.01e89162.chunk.js"
+  },
+  {
+    "revision": "282800b57e7581832cce57575ff81bb1",
+    "url": "/back-up_FrontEnd_showMeSaudi/static/js/2.01e89162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6df784c21e5959e05962",
+    "url": "/back-up_FrontEnd_showMeSaudi/static/js/main.f1a95875.chunk.js"
+  },
+  {
+    "revision": "51b588bec1cd48f0de23",
+    "url": "/back-up_FrontEnd_showMeSaudi/static/js/runtime-main.8b9526df.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/back-up_FrontEnd_showMeSaudi/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/back-up_FrontEnd_showMeSaudi/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "26d864cd7f8a96cf521ad1afa912dbf0",
+    "url": "/back-up_FrontEnd_showMeSaudi/static/media/sr.26d864cd.png"
+  }
+]);
